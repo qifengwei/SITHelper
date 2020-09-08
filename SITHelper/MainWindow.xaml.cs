d8183247da7f2044e1196cafd0845b65002ecc6c
qifengwei <fengwei.qi@outlook.com>
2020-09-08 22:34:34 +0800

    090802

--- SITHelper/MainWindow.xaml.cs
+++ SITHelper/MainWindow.xaml.cs
@@ -20,14 +20,55 @@ namespace SITHelper
     /// </summary>
     public partial class MainWindow : Window
     {
+        private UserControl uCRecord;
+        private UserControl uCSetting;
         public MainWindow()
         {
             InitializeComponent();
+            InitUserRecord();
+        }
+
+        private void InitUserRecord()
+        {
+            uCRecord = new UserControlRecord();
+            uCSetting = new UserControlSetting();
         }
 
         private void Window_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed) this.DragMove();
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Set Tooltips Visibility
+            if (tg_btn.IsChecked == true)
+            {
+                tt_record.Visibility = Visibility.Collapsed;
+            }
+            else 
+            {
+                tt_record.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void tg_btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LV_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GridMain.Children.Clear();
+            switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+            {
+                case "LVI_Record":
+                    GridMain.Children.Add(uCRecord);
+                    break;
+                case "LVI_Setting":
+                    GridMain.Children.Add(uCSetting);
+                    break;
+            }
         }
     }
 }
