704accdd7f9476ecb06c471f00cd03ebfd36439e
qifengwei <fengwei.qi@outlook.com>
2020-09-08 08:41:38 +0800

    0908

--- SITHelper/MainWindow.xaml.cs
+++ SITHelper/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace SITHelper
         {
             InitializeComponent();
         }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) this.DragMove();
+        }
     }
 }
