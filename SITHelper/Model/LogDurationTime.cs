1fb502d2d3ddfaa2992564766b67a3e74f5533d7
qifengwei <fengwei.qi@outlook.com>
2020-09-30 19:45:03 +0800

    09302

---
+++ SITHelper/Model/LogDurationTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SITHelper.Model
+{
+    public class LogDurationTime
+    {
+        public LogDurationTime()
+        {
+            PreviousTime = 240;
+            LaterTime = 60;
+        }
+
+        public int PreviousTime { get; set; }
+        public int LaterTime { get; set; }
+    }
+}
---
+++ SITHelper/Validation/TimeRangeRule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace SITHelper.Validation
+{
+    public class TimeRangeRule : ValidationRule
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var time = 0;
+            try
+            {
+                if (((string)value).Length > 0)
+                    time = int.Parse((string)value);
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, $"Illegal characters or {e.Message}");
+            }
+
+            if (time < Min || time > Max)
+            {
+                return new ValidationResult(false,
+                    $"Please enter a time in the range:{Min}-{Max}.");
+            }
+            return new ValidationResult(true, null);
+        }
+    }
+}
